6b5d2309f25af8cb603dc64165901a1f3b75f847
Mac McCann <mac.mccann@hobsons.com>
2016-07-22 09:48:49 -0400

    itsy bitsy fix to navbar (specifically, made it latch on @ window height - 5 px);

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     var wrap = $(".intro-overlay");
     var nav = $(".nav");
     var scrollTop = $(window).scrollTop();
-    var windowHeight = $(window).height();
+    var windowHeight = $(window).height() - 5;
 
     $(document).on("scroll", function(e){
         scrollTop = $(window).scrollTop();
-        windowHeight = $(window).height();
+        windowHeight = $(window).height() - 5;
         if (scrollTop > windowHeight){
             nav.addClass("fix-search");
         } else {
