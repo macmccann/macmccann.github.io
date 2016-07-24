d7978830031ac31208d36c1133d37843375526f0
macmccann <johnwilfredmccann@gmail.com>
2016-07-24 15:57:48 -0400

    padding, correct css values

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@ $(document).ready(function() {
     var wrap = $(".intro-overlay");
     var nav = $(".nav");
     var scrollTop = $(window).scrollTop();
-    var windowHeight = $(window).height() - 5;
+    var windowHeight = $(window).height() + 15;
 
     $(document).on("scroll", function(e){
         scrollTop = $(window).scrollTop();
-        windowHeight = $(window).height() - 5;
+        windowHeight = $(window).height() + 15;
         if (scrollTop > windowHeight){
             nav.addClass("fix-search");
+            console.log("this first thing");
         } else {
             nav.removeClass("fix-search");
+            console.log("this second thing");
         }
     });
 });
