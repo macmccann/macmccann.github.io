01d55f82c8653e4d86648b72428648d371d86d3c
Mac <macmccann@Macs-MacBook-Pro.local>
2024-07-15 07:31:13 -0700

    prettier; extensions -> just one extension

--- visualizers/wam_extension_butterchurn/index.js
+++ visualizers/wam_extension_butterchurn/index.js
@@ -3,10 +3,16 @@
 let synth101;
 let butterchurn;
 let hasInitialized = false;
+let audioContext = null;
+let hostKey;
+let renderer;
 
+/*=====================================================================================
+FILE AUDIO
+=======================================================================================*/
 document
-    .getElementById("file")
-    .addEventListener("change", handleFileSelect, false);
+    .getElementById('file')
+    .addEventListener('change', handleFileSelect, false);
 
 function handleFileSelect(event) {
     const file = event.target.files[0];
@@ -31,36 +37,41 @@ function initAudioFromFile(audioData) {
     });
 }
 
+/*=====================================================================================
+VIDEO EXTENSION
+=======================================================================================*/
 class VideoExtension {
     constructor() {
-        this.delegates = new Map()
-        this.connections = new Map()
+        this.delegates = new Map();
+        this.connections = new Map();
     }
 
     setDelegate(pluginId, delegate) {
         if (delegate) {
-            this.delegates.set(pluginId, delegate)
+            this.delegates.set(pluginId, delegate);
         } else {
-            this.delegates.delete(pluginId)
+            this.delegates.delete(pluginId);
         }
     }
 
     getDelegate(pluginId) {
-        return this.delegates.get(pluginId)
+        return this.delegates.get(pluginId);
     }
 }
 
 class Uniform {
     constructor(gl, program, name, suffix) {
-        this.gl = gl
-        this.program = program
+        this.gl = gl;
+        this.program = program;
         this.name = name;
         this.suffix = suffix;
         this.location = gl.getUniformLocation(program, name);
         if (this.location == -1) {
-            throw new Error("Passed name didn't correspond to an active attribute in the specified program.")
+            throw new Error(
+                "Passed name didn't correspond to an active attribute in the specified program."
+            );
         }
-        console.log(`var ${name} location ${this.location}`)
+        console.log(`var ${name} location ${this.location}`);
     }
 
     set(...values) {
@@ -75,47 +86,40 @@ class Uniform {
 
 class Rect {
     constructor(gl) {
-        this.gl = gl
+        this.gl = gl;
         this.buffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-        this.verts = new Float32Array([
-            -1, -1,
-            1, -1,
-            -1, 1,
-            1, 1,
-        ]);
+        this.verts = new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]);
         gl.bufferData(gl.ARRAY_BUFFER, this.verts, gl.STATIC_DRAW);
     }
 
     render() {
         this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
     }
-
 }
 
 export class CanvasRenderer {
-
     constructor(canvas) {
-        this.canvas = canvas
+        this.canvas = canvas;
 
-        this.gl = canvas.getContext("webgl2") ?? canvas.getContext("webgl")
+        this.gl = canvas.getContext('webgl2') ?? canvas.getContext('webgl');
 
-        this.setup(this.gl)
+        this.setup(this.gl);
     }
 
     setup(gl) {
-        console.log("CanvasRenderer: Calling setup")
+        console.log('CanvasRenderer: Calling setup');
 
         // create program
         var program = gl.createProgram();
         if (program == null) {
-            return
+            return;
         }
-        this.program = program
+        this.program = program;
 
         // add shaders
-        var vertexShaderSource = this.vertexShader()
-        var fragmentShaderSource = this.fragmentShader()
+        var vertexShaderSource = this.vertexShader();
+        var fragmentShaderSource = this.fragmentShader();
 
         this.addShader(vertexShaderSource, gl.VERTEX_SHADER);
         this.addShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
@@ -129,48 +133,56 @@ export class CanvasRenderer {
 
         // create position attrib
         this.billboard = new Rect(gl);
-        //gl.bindTexture(gl.TEXTURE_2D, this.input);
 
-        this.positionLocation = this.gl.getAttribLocation(this.program, 'a_position');
+        this.positionLocation = this.gl.getAttribLocation(
+            this.program,
+            'a_position'
+        );
 
         if (this.positionLocation < 0) {
-            console.error("positionLocation returned ", this.positionLocation)
+            console.error('positionLocation returned ', this.positionLocation);
         }
 
         this.resize();
 
-        console.log("finished setup")
+        console.log('finished setup');
     }
 
     render(input) {
         this.gl.useProgram(this.program);
-        this.gl.bindTexture(this.gl.TEXTURE_2D, input)
+        this.gl.bindTexture(this.gl.TEXTURE_2D, input);
 
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.billboard.buffer);
 
         this.gl.enableVertexAttribArray(this.positionLocation);
-        this.gl.vertexAttribPointer(this.positionLocation, 2, this.gl.FLOAT, false, 0, 0);
+        this.gl.vertexAttribPointer(
+            this.positionLocation,
+            2,
+            this.gl.FLOAT,
+            false,
+            0,
+            0
+        );
 
         this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
 
         this.billboard.render();
 
-        this.gl.bindTexture(this.gl.TEXTURE_2D, null)
+        this.gl.bindTexture(this.gl.TEXTURE_2D, null);
         this.gl.useProgram(null);
-
     }
 
-    // ----- resize ----- //    
+    // ----- resize ----- //
     resize() {
-        var width = 640
-        var height = 480
+        var width = 640;
+        var height = 480;
 
         this.uResolution.set(width, height);
         this.gl.viewport(0, 0, width, height);
     }
 
     addShader(source, type) {
-        let gl = this.gl
+        let gl = this.gl;
         var shader = gl.createShader(type);
         if (shader == null) {
             throw new Error('createShader returned null');
@@ -179,11 +191,12 @@ export class CanvasRenderer {
         gl.compileShader(shader);
         var isCompiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
         if (!isCompiled) {
-            throw new Error('Shader compile error: ' + gl.getShaderInfoLog(shader));
+            throw new Error(
+                'Shader compile error: ' + gl.getShaderInfoLog(shader)
+            );
         }
         gl.attachShader(this.program, shader);
     }
-
 
     vertexShader() {
         return `
@@ -192,7 +205,7 @@ attribute vec2 a_position;
 void main() {
     gl_Position = vec4(a_position, 0, 1);
 }
-        `
+        `;
     }
 
     fragmentShader() {
@@ -207,38 +220,34 @@ void main() {
 
     gl_FragColor = texture2D(texture, pos);
 }
-    `
+    `;
     }
 }
 
 const AudioCtx = window.AudioContext || window.webkitAudioContext;
 
 const setUIStateForLoading = () => {
-    document.getElementById("init-button").src = "./assets/loading.svg";
-    document.getElementById("init").style.cursor = "wait";
-}
+    document.getElementById('init-button').src = './assets/loading.svg';
+    document.getElementById('init').style.cursor = 'wait';
+};
 
-let audioContext = null;
-document.getElementById("init").addEventListener("click", () => {
+document.getElementById('init').addEventListener('click', () => {
     if (hasInitialized) return;
     hasInitialized = true;
     const init = async () => {
         setUIStateForLoading();
         audioContext = new AudioCtx();
         await run();
-        document.getElementById("init").style.display = 'none';
-    }
+        document.getElementById('init').style.display = 'none';
+    };
     init();
 });
 
-
-let hostKey;
-let renderer;
-let videoPlugins = [];
-
 const initHost = async (audioContext) => {
-    const { default: initializeWamHost } = await import("https://www.webaudiomodules.com/sdk/2.0.0-alpha.6/src/initializeWamHost.js");
-    const [, key] = await initializeWamHost(audioContext, "example");
+    const { default: initializeWamHost } = await import(
+        'https://www.webaudiomodules.com/sdk/2.0.0-alpha.6/src/initializeWamHost.js'
+    );
+    const [, key] = await initializeWamHost(audioContext, 'example');
     hostKey = key;
 
     await initExtensions();
@@ -247,7 +256,7 @@ const initHost = async (audioContext) => {
 const initExtensions = async () => {
     window.WAMExtensions = window.WAMExtensions || {};
     window.WAMExtensions.video = new VideoExtension();
-}
+};
 
 // Load a WebAudioModule, and return an instance.
 async function loadWAM(path) {
@@ -255,133 +264,78 @@ async function loadWAM(path) {
     const { default: WAM } = await import(path);
 
     if (typeof WAM !== 'function' || !WAM.isWebAudioModuleConstructor) {
-        throw new Error(`Path ${path} is not a WebAudioModule.`)
-    };
+        throw new Error(`Path ${path} is not a WebAudioModule.`);
+    }
 
     // if you wish to load more instances of a WAM, best to keep a reference to the WAM class
     // instead of importing it again.
-    const instance = new WAM("example", audioContext)
-    await instance.initialize(initialState)
+    const instance = new WAM('example', audioContext);
+    await instance.initialize(initialState);
 
     return instance;
 }
 
 async function initVideo(context) {
-    let canvas = document.getElementById("wam-video")
+    let canvas = document.getElementById('wam-video');
     renderer = new CanvasRenderer(canvas);
 }
 
 async function run() {
-
     await initHost(audioContext);
 
     await initVideo(audioContext);
 
-    butterchurn = await loadWAM("https://www.webaudiomodules.com/community/plugins/burns-audio/video_butterchurn/index.js")
-    synth101 = await loadWAM('./lib/synth101/index.js')
+    butterchurn = await loadWAM(
+        'https://www.webaudiomodules.com/community/plugins/burns-audio/video_butterchurn/index.js'
+    );
+    synth101 = await loadWAM('./lib/synth101/index.js');
 
     synth101.audioNode.connect(butterchurn.audioNode);
     butterchurn.audioNode.connect(audioContext.destination);
 
     // create the UI and add it to the container
-    const ui = await butterchurn.createGui()
-    const container = document.getElementById("instrument-container")
-    container.appendChild(ui)
+    const ui = await butterchurn.createGui();
+    const container = document.getElementById('instrument-container');
+    container.appendChild(ui);
 
-    videoPlugins.push(butterchurn);
-
-    // could add more video plugins here
-
-    let extensions = videoPlugins.map(v => window.WAMExtensions.video.getDelegate(v.instanceId))
+    let butterchurnExtension = window.WAMExtensions.video.getDelegate(
+        butterchurn.instanceId
+    );
 
     let videoOptions = {
         width: 640,
         height: 480,
-        gl: renderer.gl
-    }
-
-    for (let e of extensions) {
-        console.log(e)
-        e.connectVideo(videoOptions);
-    }
+        gl: renderer.gl,
+    };
+    butterchurnExtension.connectVideo(videoOptions);
 
     window.requestAnimationFrame(renderVideo);
 }
 
 function renderVideo() {
-    let extensions = videoPlugins.map(v => window.WAMExtensions.video.getDelegate(v.instanceId))
+    let butterchurnExtension = window.WAMExtensions.video.getDelegate(
+        butterchurn.instanceId
+    );
 
-    let inputs = []
-    let currentTime = audioContext.currentTime
+    let inputs = [];
+    let currentTime = audioContext.currentTime;
 
-    for (let e of extensions) {
-        inputs = e.render(inputs, currentTime);
-    }
+    inputs = butterchurnExtension.render(inputs, currentTime);
 
-    renderer.render(inputs[0])
+    renderer.render(inputs[0]);
 
     window.requestAnimationFrame(renderVideo);
-
 }
 
-document.getElementById("note").addEventListener("mousedown", () => {
-    synth101.audioNode.scheduleEvents({ type: 'wam-midi', time: synth101.audioNode.context.currentTime, data: { bytes: new Uint8Array([0x90, 74, 100]) } });
-    synth101.audioNode.scheduleEvents({ type: 'wam-midi', time: synth101.audioNode.context.currentTime + 0.25, data: { bytes: new Uint8Array([0x80, 74, 100]) } });
-})
-
-console.log("=============== START ================")
-
-
-function resetGLContext(gl, isWebGL2) {
-    // reset state
-    // this code was copied from mrdoob/threejs, license MIT
-
-    gl.disable(gl.BLEND);
-    gl.disable(gl.CULL_FACE);
-    gl.disable(gl.DEPTH_TEST);
-    gl.disable(gl.POLYGON_OFFSET_FILL);
-    gl.disable(gl.SCISSOR_TEST);
-    gl.disable(gl.STENCIL_TEST);
-    gl.disable(gl.SAMPLE_ALPHA_TO_COVERAGE);
-
-    gl.blendEquation(gl.FUNC_ADD);
-    gl.blendFunc(gl.ONE, gl.ZERO);
-    gl.blendFuncSeparate(gl.ONE, gl.ZERO, gl.ONE, gl.ZERO);
-
-    gl.colorMask(true, true, true, true);
-    gl.clearColor(0, 0, 0, 0);
-
-    gl.depthMask(true);
-    gl.depthFunc(gl.LESS);
-    gl.clearDepth(1);
-
-    gl.stencilMask(0xffffffff);
-    gl.stencilFunc(gl.ALWAYS, 0, 0xffffffff);
-    gl.stencilOp(gl.KEEP, gl.KEEP, gl.KEEP);
-    gl.clearStencil(0);
-
-    gl.cullFace(gl.BACK);
-    gl.frontFace(gl.CCW);
-
-    gl.polygonOffset(0, 0);
-
-    gl.activeTexture(gl.TEXTURE0);
-
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-
-    if (isWebGL2 === true) {
-
-        // @ts-ignore
-        gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
-        // @ts-ignore
-        gl.bindFramebuffer(gl.READ_FRAMEBUFFER, null);
-
-    }
-
-    gl.useProgram(null);
-
-    gl.lineWidth(1);
-
-    gl.scissor(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-}
+document.getElementById('note').addEventListener('mousedown', () => {
+    synth101.audioNode.scheduleEvents({
+        type: 'wam-midi',
+        time: synth101.audioNode.context.currentTime,
+        data: { bytes: new Uint8Array([0x90, 74, 100]) },
+    });
+    synth101.audioNode.scheduleEvents({
+        type: 'wam-midi',
+        time: synth101.audioNode.context.currentTime + 0.25,
+        data: { bytes: new Uint8Array([0x80, 74, 100]) },
+    });
+});
